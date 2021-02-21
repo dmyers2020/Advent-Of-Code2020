5172e2ea651822a767ccabcc0834fbec8ef1423d
David <nothanksbox@gmail.com>
2021-02-20 21:37:34 -0800

    initial approach thoughts, kids crying. had to abandon

--- 7/code.py
+++ 7/code.py
@@ -4,6 +4,18 @@
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
+'''
+initial thoughts: taking a peek at the input tells shows that there's a lot more than just the 9 example rules they provided.
+i'm thinking I need to:
+ - split the input on newline '\n'
+  - define a 'rule' class
+    attributes:
+        self_description (color_mod, color)
+        content_description (quantity, color_mod, color)
+ - create objects for each rule in input (list comp)
+
+'''
+    input=input.split('\n')
 
 
 
