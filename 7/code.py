aece588a3583eb670af54577b37cc062878e92e8
David <nothanksbox@gmail.com>
2021-02-21 10:00:20 -0800

    did some thinking; no coding - restart the clock

--- 7/code.py
+++ 7/code.py
@@ -2,22 +2,36 @@
 # Author = David Myers
 # Date = December 2020
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+with open((__file__.rstrip("code.py")+"sample_input.txt"), 'r') as input_file:
     input = input_file.read()
 '''
 initial thoughts: taking a peek at the input tells shows that there's a lot more than just the 9 example rules they provided.
 i'm thinking I need to:
- - split the input on newline '\n'
-  - define a 'rule' class
+ - split the input on newline '\n' to separate rules
+ - split each rule on ',' to get list of contents
+ - keyword 'contain' preceeds allowed quantity of each kind of contents
+    - quantity special case: 'no other'
+    - quantity otherwise: regex number in  each content
+  - define a 'bag' class?
     attributes:
         self_description (color_mod, color)
         content_description (quantity, color_mod, color)
- - create objects for each rule in input (list comp)
+ - create objects for each bag in input (list comp?)
+
+ how many colors can, eventually, contain at least one shiny gold bag?
+    - we only count the OUTTERMOST BAG, using sample_input, the answer should be 4
+
+ - a directed graph using tuples?
+
+ - note that in part 1, it doesn't seem that the quantities given in the rules,
+  have any relevance; so they probably DO in part 2, as costs for each arc
+
+  - rule pattern: 'color_mod color useless [qty, no] color_mod color, [[qty, no] color_mod color]
 
 '''
-    input=input.split('\n')
+input =input.split('\n')
 
-
+print(input)
 
 print("Part One : "+ str(None))
 
